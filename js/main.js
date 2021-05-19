fce411240b3f2ad417c81600fb3c005f73a53f7d
sergkoshdv <sergkoshelevich@gmail.com>
2021-05-20 02:41:16 +1000

    create: responsive slider

--- js/main.js
+++ js/main.js
@@ -21,4 +21,25 @@ const swiper = new Swiper('.swiper-container', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 767px
+    767: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 1,
+    }
+  }
 });
